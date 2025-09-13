1eea3c0aa50d4def26e993a5cace9259e85d8e7f
olucascdev <lucasdevproject@gmail.com>
2025-09-12 23:01:59 -0300

    Done! Exercise Cash

--- c/cash.c
+++ c/cash.c
@@ -9,15 +9,34 @@ int main(void)
         printf("Troco Devido: ");
         scanf("%f", &troco);
     }
-    while (troco < 0);
+    while (troco <= 0);
 
-    int centavos = round(troco * 100);
+    troco = round(troco * 100);
     int moedas = 0;
 
-    if(centavos >=25)
+    while(troco >= 25)
     {
-        moedas += centavos / 25;
-        centavos = centavos % 25;
+        moedas++;
+        troco = troco - 25;
     }
+
+    while(troco >= 10)
+    {
+        moedas++;
+        troco = troco - 10;
+    }
+    
+    while(troco >= 5)
+    {
+        moedas++;
+        troco = troco - 5;
+    }
+
+    while(troco >= 1)
+    {
+        moedas++;
+        troco = troco - 1;
+    }
+
     printf("moedas: %i\n", moedas);
 }
